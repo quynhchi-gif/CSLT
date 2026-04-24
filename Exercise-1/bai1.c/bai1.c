78e6a686f944ee8d807c8ff89b05f3c41c26aac6
quynhchi-gif <haquynhchi160820@gmail.com>
2026-04-24 17:14:17 +0700

    Create bai1.c

---
+++ Exercise-1/bai1.c/bai1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int a, b, sum;
+    printf("Nhập hai số nguyên: ");
+    scanf("%d %d, &a, &b);
+    sum = a + b;
+    printf("Tổng là: %d", sum);
+    return 0;
+}
